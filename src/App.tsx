064554ef7f6224d379ef3560a504d72f6120bf34
Pavani-chowla <83864319+Pavani-chowla@users.noreply.github.com>
2023-01-06 11:14:55 +0530

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,9 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if(this.state.showGraph){
+         return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
@@ -40,12 +42,19 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
     let x = 0;
-        const interval = setInterval(() => {
+    const interval = setInterval(() => {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data:serverResponds,showGraph:true });
+      this.setState({ data:serverResponds,
+                     showGraph:true 
+                    });
     });
+    x++;
+    if(x>1000){
+      clearInterval(interval);
+    }
+  },100);
   }
 
   /**
